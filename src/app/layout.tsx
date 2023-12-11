acb17f3293175be5275d0d0cb0ac6e7e9915c826
Shoxrux Code <shoxruxpl04@gmail.com>
2023-12-11 09:46:28 -0500

    layout was successfully added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
+import { Header } from '@/components/Header'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Footer } from '@/components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,7 +19,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Header></Header>
+        <div className='w-full h-screen'>
         {children}
+        </div>
+        <Footer></Footer>
         </body>
     </html>
   )
